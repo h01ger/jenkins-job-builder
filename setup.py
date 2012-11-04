ca4e5c902172bd61a9f03dfe085891e05566c20f
Holger Levsen <holger@layer-acht.org>
2012-11-04 02:25:00 +0100

    add support for sidebar-properties

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setuptools.setup(
             'authenticated-build=jenkins_jobs.modules.properties:'
             'authenticated_build',
             'authorization=jenkins_jobs.modules.properties:authorization',
+            'sidebar=jenkins_jobs.modules.properties:sidebar',
         ],
         'jenkins_jobs.parameters': [
             'string=jenkins_jobs.modules.parameters:string_param',
